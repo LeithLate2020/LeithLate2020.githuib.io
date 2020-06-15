e5318e4f5028158f026a8ded8422d815c1539781
LeithLate2020 <66818621+LeithLate2020@users.noreply.github.com>
2020-06-15 12:53:21 +0100

    Add files via upload

---
+++ JS/studio_data5.js
@@ -0,0 +1,41 @@
+var data = {
+    "type" : "FeatureCollection",
+    "name" : "data",
+    "features" : [
+    
+        { 
+            "type" : "Feature", 
+            "properties" : {  
+                "name" : "Out of the Blue", 
+                "text" : "<p><b>A short film about everything that the Leith School of Art has to offer. Produced by Peter Keith Films.</b></p>",
+                "media" : "<iframe src=\"https://www.youtube.com/embed/aCv9zGIXdEY\" frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>",
+                "artists" : "<p>Artists</p><div class=\"artist_name\"><a href=\"https://www.leithlate.co.uk/eddie1-gallery\">Allan Ramsay</a></div>"
+            }, 
+            "geometry" : { 
+                "type" : "Point", 
+                "coordinates" : [
+                	240,
+                    356
+                ] 
+            }
+        },
+
+        { 
+            "type" : "Feature", 
+            "properties" : {  
+                "name" : "Custom Lane", 
+                "text" : "<p><b>Leith School of Art (LSA) is an independent art college in Edinburgh, Scotland, providing tertiary education in art and design.</b></p>",
+                "media" : "<div id=\"malihu3\" style=\"overflow: auto; width: 80%; max-width: 300px; height: auto;\"><ul><li><img src=\"Images/lsa1_thumb.jpg\" /></li><li><img src=\"Images/lsa2_thumb.jpg\" /></li><li><img src=\"Images/lsa3_thumb.jpg\" ></li></ul></div>",
+                "artists" : "<p>Artists</p><div class=\"artist_name\">Richard Demarco</div><br/><div class=\"artist_name\">William Bonnar</div>"
+            }, 
+            "geometry" : { 
+                "type" : "Point", 
+                "coordinates" : [
+                    305,
+                    692
+                ] 
+            }
+        }
+
+    ]
+};
