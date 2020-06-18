6f4e342ee892f00093b37d7d8d923a69e1663c22
LeithLate2020 <66818621+LeithLate2020@users.noreply.github.com>
2020-06-18 09:18:37 +0100

    Add files via upload

---
+++ JS/studio_data6.js
@@ -0,0 +1,44 @@
+var data = {
+    "type" : "FeatureCollection",
+    "name" : "data",
+    "features" : [
+
+
+        { 
+            "type" : "Feature", 
+            "properties" : {  
+                "name" : "Custom Lane", 
+                "studio_text" : "<p>Custom Lane is an evolving, collaborative experiment in creating the most engaging, enriching and enjoyable work environment for designers in Scotland.</p>",
+                "media" : "<div id=\"malihu3\" style=\"overflow: auto; width: 80%; max-width: 300px; height: auto;\"><ul><li><img src=\"Images/lsa1_thumb.jpg\" /></li><li><img src=\"Images/lsa2_thumb.jpg\" /></li><li><img src=\"Images/lsa3_thumb.jpg\" ></li></ul></div>",
+                "artists" : "<img src=\"Images/ar.jpg\" class=\"artist\" onclick=\"populate_sidebar(0)\"><br/>Allan Ramsay",
+                "artist_info" : {"artist1" : "<div class=\"content\"><img src=\"Images/ar.jpg\" class=\"artist\">Allan Ramsay was born in Edinburgh, Scotland, the eldest son of Allan Ramsay, poet and author of The Gentle Shepherd. From the age of twenty he studied in London under the Swedish painter Hans Hysing, and at the St. Martin's Lane Academy; leaving in 1736 for Rome and Naples, where he worked for three years under Francesco Solimena and Imperiali (Francesco Fernandi). <a href=\"https://en.wikipedia.org/wiki/Allan_Ramsay_(artist)\">Wikipedia entry</a></div><div id=\"malihu3\" style=\"overflow: auto; width: 80%; max-width: 300px; height: auto;\"><ul><li><img src=\"Images/lsa1_thumb.jpg\" /></li><li><img src=\"Images/lsa2_thumb.jpg\" /></li><li><img src=\"Images/lsa3_thumb.jpg\" ></li></ul></div>"}
+            }, 
+            "geometry" : { 
+                "type" : "Point", 
+                "coordinates" : [
+                    181,
+                    415
+                ] 
+            }
+        },
+
+        { 
+            "type" : "Feature", 
+            "properties" : {  
+                "name" : "Out of the Blue", 
+                "studio_text" : "<p>Out of the Blue Arts and Education Trust is based in the Out of the Blue Drill Hall in Dalmeny Street, Edinburgh.</p>",
+                "media" : "<iframe src=\"https://www.youtube.com/embed/aCv9zGIXdEY\" frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>",
+                "artists" : "<img src=\"Images/wb.jpg\" class=\"artist\" onclick=\"populate_sidebar(0)\">William Bonnar<br/><img src=\"Images/rd.jpg\" class=\"artist\" onclick=\"populate_sidebar(1)\">Richard Demarco",
+                "artist_info" : {"artist1" : "<div class=\"content\"><img src=\"Images/wb.jpg\" class=\"artist\">Bonnar was born in Edinburgh in 1800. His father was Thomas Bonnar a house-painter and interior designer of considerable skill. Bonnar showed an early aptitude for drawing, and was apprenticed to one of the leading decorators of the time. When George IV visited Edinburgh in 1822, Bonnar assisted David Roberts in decorating the Assembly Rooms for the grand state ball which was given in honour of the occasion. Shortly afterwards some signboards painted by him attracted the notice of Captain Basil Hall, who sought out and encouraged the young artist. In the year 1824 his picture of 'The Tinkers ' established him as a favourite with the public, and shortly after the formation of the Royal Scottish Academy in 1830 he was elected one of its members. <a href=\"https://en.wikipedia.org/wiki/William_Bonnar\">Wikipedia entry</a><br/><iframe src=\"https://www.youtube.com/embed/aCv9zGIXdEY\" frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe></div>", "artist2" : "<div class=\"content\"><img src=\"Images/rd.jpg\" class=\"artist\">Demarco was a co-founder of the Traverse Theatre in Edinburgh in 1963. Three years later he and other organisers of the theatre's gallery space left the Traverse to establish what became the Richard Demarco Gallery. The gallery, which doubled as a performance venue during the Edinburgh Fringe, ran from 1966 to 1992. <a href=\"https://en.wikipedia.org/wiki/Richard_Demarco\">Wikipedia entry</a><br/><br/>Audio of Artist:<br/><audio controls=\"controls\"><source src=\"Audio/rd1.mp3\" type=\"audio/mpeg\">Your browser does not support the audio element.</audio></div>"}
+            }, 
+            "geometry" : { 
+                "type" : "Point", 
+                "coordinates" : [
+                	143,
+                    214
+                ] 
+            }
+        }
+
+    ]
+};
